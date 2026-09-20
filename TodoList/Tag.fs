357c171ab75a7c87286eb3cac61b10bf4adea336
Percyqaz <percyqaz@yahoo.co.uk>
2026-09-20 15:38:07 +0200

    feat: all-caps days like @MONDAY etc macro to the next occurence of that day

--- TodoList/Tag.fs
+++ TodoList/Tag.fs
@@ -1,6 +1,7 @@
 namespace Tedium
 
 open System
+open System.Globalization
 
 [<Struct>]
 type Tag =
@@ -13,6 +14,30 @@ type Tag =
         match this.Value with
         | ValueSome v -> sprintf "@%s:%s" this.Label v
         | ValueNone -> sprintf "@%s" this.Label
+
+    static member ParseSpecial(name: string) : Tag voption =
+
+        let date_of_value (value: string) : Tag voption =
+            ValueSome { Label = "date"; Value = ValueSome(value) }
+
+        let day_of_week (day: DayOfWeek) : Tag voption =
+            let mutable today = DateOnly.FromDateTime(DateTime.Today).AddDays(1)
+
+            while today.DayOfWeek <> day do
+                today <- today.AddDays(1)
+
+            date_of_value(today.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture))
+
+        match name with
+        | "TODAY" -> date_of_value(DateTime.Today.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture))
+        | "SUNDAY" -> day_of_week(DayOfWeek.Sunday)
+        | "MONDAY" -> day_of_week(DayOfWeek.Monday)
+        | "TUESDAY" -> day_of_week(DayOfWeek.Tuesday)
+        | "WEDNESDAY" -> day_of_week(DayOfWeek.Wednesday)
+        | "THURSDAY" -> day_of_week(DayOfWeek.Thursday)
+        | "FRIDAY" -> day_of_week(DayOfWeek.Friday)
+        | "SATURDAY" -> day_of_week(DayOfWeek.Saturday)
+        | _ -> ValueNone
 
     static member TryParse(value: string, out: outref<Tag>) : bool =
         let inline acceptable_tag_char (c: char) : bool =
@@ -28,7 +53,11 @@ type Tag =
             let split = value.Substring(1).Split(TAG_DATA_SEPARATOR, 2)
 
             if not(String.forall acceptable_tag_char split.[0]) then
-                false
+                match Tag.ParseSpecial(split.[0]) with
+                | ValueSome special ->
+                    out <- special
+                    true
+                | ValueNone -> false
             elif split.Length > 1 then
                 if not(String.forall acceptable_data_char split.[1]) then
                     false
